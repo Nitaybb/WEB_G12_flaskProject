ff47f58bb940f4b4f5ddef12e6dc89e1d4297fac
shahar vazana <vazsha@post.bgu.ac.il>
2022-02-18 16:12:33 +0200

    review not done

--- pages/reviews/reviews.py
+++ pages/reviews/reviews.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
+import mysql.connector
+from utilities.classes.review import reviews
 
 # products blueprint definition
 reviews = Blueprint('reviews', __name__, static_folder='static', static_url_path='/reviews', template_folder='templates')
@@ -8,3 +10,14 @@ reviews = Blueprint('reviews', __name__, static_folder='static', static_url_path
 @reviews.route('/reviews')
 def index():
     return render_template('reviews.html')
+
+
+@reviews.route('/request', methods=['GET', 'POST'])
+def main_func():
+    if request.method == 'POST':
+        rv = reviews()
+        rv.name = request.form['inputName']
+        rv.Comment = request.form['Comment']
+        # rv.stars_rate = request.form['stars_rate']
+        rv.info()
+    return redirect('/reviews')
--- utilities/classes/review.py
+++ utilities/classes/review.py
@@ -1,8 +1,17 @@
 from utilities.db.db_manager import dbManager
 
+
 class reviews:
-    def __init__(self, review_ID=None, name=None, Comment=None, stars_rate=None):
-        self.review_ID = review_ID
+    def __init__(self, name=None, Comment=None):
+        self.review_ID = 1
         self.name = name
         self.Comment = Comment
-        self.stars_rate = stars_rate
+        self.stars_rate = 4
+
+    def info(self):
+        query = "INSERT INTO reviews(review_ID, name, Comment, stars_rate) VALUES ('%s', '%s', '%s', '%s') % (self.review_ID, self.name, self.Comment, self.stars_rate)"
+        dbManager.commit(query)
+        # dbManager.commit('''
+        # INSERT INTO reviews(review_ID, name, Comment, stars_rate)
+        # VALUES ('%s', '%s', '%s', '%s')
+        # ''' % (self.review_ID, self.name, self.Comment, self.stars_rate))
